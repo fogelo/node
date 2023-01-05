30daeb72b7e2f88ed63eaeaeac010757461ae83e
fogelo <fogelo@yandex.ru>
2023-01-06 00:44:01 +0300

    os

---
+++ os.js
@@ -0,0 +1,5 @@
+const os = require("os")
+
+console.log(os.platform()) // получение текуще ос
+console.log(os.arch()) //архетиктура процессора
+console.log(os.cpus()) //информация о процессоре и его ядрах
